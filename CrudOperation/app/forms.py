309c1854af6a1aefd5aa6dacb98b28b0871490da
Jyothish Kumar <JyothishElanjimattom@gmail.com>
2022-02-08 10:01:55 +0530

    added models and form

---
+++ CrudOperation/app/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from models import StudntRegistration
+
+class StudentRegForm(forms.ModelForm):
+    class Meta:
+        model = StudntRegistration
+        fields = ['name','email','department','mobno']
--- CrudOperation/app/models.py
+++ CrudOperation/app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class StudntRegistration(models.Model):
+    name = models.CharField(max_length=45)
+    email = models.EmailField(max_length=45)
+    department = models.CharField(max_length=23)
+    mobno= models.IntegerField()
